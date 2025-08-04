562b8f73a3cfb1a61e17f54271b3694417afb4b9
Kayin Ilagan <kayinilagan@gmail.com>
2025-08-03 23:18:01 -0400

    Adding JS and began modal

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+
+
+
+function setName() {
+    const nameDialog = document.querySelector('#name-dialog');
+    nameDialog.showModal();
+}
+
+function setBackground() {
+    console.log('setBackground');
+}
+
+function setInventory() {
+    console.log('setInventory');
+}
+
+function setPetty() {
+    console.log('setPetty');
+}
+
+function setStats() {
+    console.log('setStats');
+}
+
+function setBonds() {
+    console.log('setBonds');
+}
+
+function setOmens() {
+    console.log('setOmens');
+}
+
